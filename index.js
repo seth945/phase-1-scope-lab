d3757a24e53867a0d18a9f37fbae39697b1ce24e
seth baraka <barakasetho5@gmail.com>
2025-03-30 20:07:58 +0300

    initail document

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob'
+
+function getCustomerName() {
+    return customerName
+}
+console.log(getCustomerName())
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+upperCaseCustomerName()
+console.log(customerName)
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; 
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "some customer"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "another customer"
+}
